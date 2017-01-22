37ddaae71bff3cdc156f38c25506489ddab7b719
Colm Seale <colm.seale@gmail.com>
2017-01-22 19:57:22 +0000

    added CommitCommentEvent

--- src/components/Changelog/index.js
+++ src/components/Changelog/index.js
@@ -35,6 +35,8 @@ export default class Changelog extends Component {
 
   generateMessage(item) {
     switch(item.type) {
+      case 'CommitCommentEvent':
+        return `Commented on commit ${item.payload.commit_id} [${item.repo.name}](${item.repo.url})`
       case 'CreateEvent':
         return `Created branch ${item.payload.ref} on [${item.repo.name}](${item.repo.url})`
       case 'DeleteEvent':
